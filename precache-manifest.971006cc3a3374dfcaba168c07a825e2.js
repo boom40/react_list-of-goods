61803b687c19ddf49ce740326c28ccb98cee7d2b
boom40 <kickgames675@gmail.com>
2019-12-10 14:36:09 +0200

    Updates

---
+++ precache-manifest.971006cc3a3374dfcaba168c07a825e2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691f2a88d72abf00fd7e369e16e694bb",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "5a24a4d87377b5283f3c",
+    "url": "/react_list-of-goods/static/js/2.4a80f5eb.chunk.js"
+  },
+  {
+    "revision": "1970b86e6edff0640069",
+    "url": "/react_list-of-goods/static/js/main.56f3c727.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.f66474f2e4306c70e9c925ca11979956.js"
+  "/react_list-of-goods/precache-manifest.971006cc3a3374dfcaba168c07a825e2.js"
 );
 
 self.addEventListener('message', (event) => {
